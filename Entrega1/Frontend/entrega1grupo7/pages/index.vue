7c51e67546d7ffff09e97d56bd7cc54ec22a3c96
Luciano Banda <luciano.banda@usach.cl>
2021-05-29 18:34:01 -0400

    [Diego Alvarado, Andrés Araya, Luciano Banda, Arturo Cadenas, Daniel Jara, Aylin Rodríguez, Jennifer Velozo] -[Se mejora forma de acceder a los datos en el front]

--- Entrega1/Frontend/entrega1grupo7/pages/index.vue
+++ Entrega1/Frontend/entrega1grupo7/pages/index.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="container">
+
+    <h1>Buscar voluntario</h1>
+    <form>
+        <div class="form-item">
+            <label for="id_voluntario">ID voluntario</label>
+            <input type="number" id="id_voluntario" v-model="id_voluntario" name="id_voluntario">
+        </div>
+        <div>
+            <button type="button" v-on:click="getData(id_voluntario)" class="main"> Buscar</button>
+        </div>
+    </form>
+    
+
     <h1>Tareas del voluntario</h1>
     <ul class="item-list">
       <li v-for="(item, index) in items" :key="index">
@@ -10,13 +23,13 @@
         Fecha de Inicio: {{item.finicio}}
         Fecha de Término: {{item.ffin}}</p>
         <p>
-        <button type="button" @click="send" class="main">Aceptar</button>
+        <button type="button" v-on:click="send(item.id, id_voluntario)" class="main">Aceptar</button>
         Rechazar
         Cancelar
         Terminar
-        </p>
-        
+        </p>  
       </li>
+
     </ul>
     <div v-if="items.length==0" class="empty-list">
       <em>No se han cargado los datos</em>
@@ -33,27 +46,40 @@ export default {
         }
     },
     methods:{
+
+        
         //Función asíncrona para consultar los datos
-        getData: async function(){
+        getData: async function(id_voluntario){
+
+            
+            
             try {
-                let response = await this.$axios.get('/tareas/824');
+                //let response = await this.$axios.get('/tareas/{{item.id}}');
+                //console.log('get data');
+                console.log(id_voluntario);
+                let response = await this.$axios.get('/tareas/' + id_voluntario);
                 this.items  = response.data;
                 console.log(response)
             } catch (error) {
                 console.log('error', error);
             }
         },
-        send: async function(){
+        send: async function(id_tarea, id_voluntario){
             this.message = '';
             //envío de datos del formulario
             console.log('Entre funcion boton')
             try {
-                console.log('Entra al try')
-                var result = await this.$axios.put('/rankings/824/48/2');
+                console.log(id_tarea)
+                //var result = await this.$axios.put('/rankings/824/{id_tarea}/2');
+                var result = await this.$axios.put('/rankings/' + id_voluntario + '/' + id_tarea + '/2');
                 console.log('Pasa var result')
                 let ranking = result.data;
                 //mensaje de exito 
                 this.message = `Se actualizó el ranking con flg_participa: ${ranking.flg_participa}`;
+
+                //Actualizar página
+                location. reload('/rankings/' + id_voluntario)
+                
             } catch (error) {
                 console.log('entro al error')
                 //mensaje de error
